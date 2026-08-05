05ab50361e082afb83a9155016743522227893ca
Lee Leighnard Jose <bilibidbinilibid@gmail.com>
2026-08-05 08:50:19 +0800

    Vercel test

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT
-const apikey = process.env.MOVIEKEY
 const movieRoutes = require('./routes/movieRouter');
 const cors = require('cors');
 
@@ -11,6 +9,14 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/movies', movieRoutes);
 
-app.listen(port, () => {
-    console.log(`Connected to port ${port}`)
-})
+app.get('/', (req, res) => res.status(200).send('Movie API is running'));
+
+module.exports = app;
+
+// Only start a local server when not running on Vercel serverless
+if (process.env.VERCEL !== '1') {
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`Connected to port ${port}`)
+    })
+}
